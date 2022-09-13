295eb82e20b537bb1226c73423d2522bb57ce573
Liza <elizaveta.b.elizaveta@yandex.by>
2022-09-13 19:57:44 +0300

    задание 1

---
+++ задание1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int rows = ReadInt ("Введите количество строк: ");
+int columns = ReadInt ("Введите количество столбцов: ");
+int[,] numbers = new int [rows, columns];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10,11);
+        }
+    }
+}
+
+int ReadInt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
